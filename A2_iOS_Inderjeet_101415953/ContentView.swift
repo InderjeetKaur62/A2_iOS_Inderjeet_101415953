9da4f5f8977141bee7bc4deadcacc3bdee1403d0
InderjeetKaur62 <101415953@georgebrown.ca>
2025-03-28 12:50:47 -0400

    Basic struc

--- A2_iOS_Inderjeet_101415953/ContentView.swift
+++ A2_iOS_Inderjeet_101415953/ContentView.swift
@@ -1,3 +1,34 @@
 import SwiftUI
 import CoreData
 
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        entity: Product.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Product.name, ascending: true)]
+    ) var products: FetchedResults<Product>
+    
+    @State private var searchText = ""
+    @State private var currentIndex = 0
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
